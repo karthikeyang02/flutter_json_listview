d74b849be08d41689d0f11e1739d4ca8ea2890b6
karthikeyan G <karthikeyang463@gmail.com>
2020-09-19 12:54:11 +0530

    added a comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+//just something
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -18,26 +20,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
-
+class _MyHomePageState extends State<MyHomePage> {
   String url = 'https://randomuser.me/api/?results=15';
   List data;
 
-  Future<String> makeRequest() async{
-    var response =
-    await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+  Future<String> makeRequest() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
 
-  /*  *//*print(response.body);*//*
+    /*  */ /*print(response.body);*/ /*
     setState((){
       var extractdata = jsonDecode(response.body);
       data = extractdata["results"];
 
-      *//*print(data[0]["name"]["first"]);*//*
+      */ /*print(data[0]["name"]["first"]);*/ /*
       print(data[0]["name"]);
     });*/
 
@@ -59,60 +60,53 @@ class _MyHomePageState extends State<MyHomePage>{
         title: new Text("Contact List"),
       ),
       body: new ListView.builder(
-          itemCount: data == null? 0: data.length,
-          itemBuilder: (BuildContext context, i){
+          itemCount: data == null ? 0 : data.length,
+          itemBuilder: (BuildContext context, i) {
             return new ListTile(
               title: new Text(data[i]["name"]["first"]),
               subtitle: new Text(data[i]["phone"]),
               leading: new CircleAvatar(
-                backgroundImage: new NetworkImage(data[i]["picture"]["thumbnail"]),
+                backgroundImage:
+                    new NetworkImage(data[i]["picture"]["thumbnail"]),
               ),
-              onTap: (){
+              onTap: () {
                 Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                  new SecondPage(data[i])));
+                    context,
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            new SecondPage(data[i])));
               },
             );
           }),
-
     );
   }
 }
 
-class SecondPage extends StatelessWidget{
+class SecondPage extends StatelessWidget {
   SecondPage(this.data);
   final data;
 
   @override
   Widget build(BuildContext context) {
-   return new Scaffold(
-     appBar: new AppBar(
-       title: new Text("Second Page"),
-     ),
-
-     body: new Center(
-       child: new Container(
-         width: 150.0,
-           height: 150.0,
-         decoration: new BoxDecoration(
-           color: const Color(0xff7c94b6),
-           image: new DecorationImage(
-               image: new NetworkImage(data["picture"]["large"]),
-                fit: BoxFit.cover,
-           ),
-           borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
-           border: new Border.all(
-             color: Colors.red,
-             width: 4.0
-           ),
-         ),
-       ),
-     ),
-   );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Second Page"),
+      ),
+      body: new Center(
+        child: new Container(
+          width: 150.0,
+          height: 150.0,
+          decoration: new BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: new DecorationImage(
+              image: new NetworkImage(data["picture"]["large"]),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
+            border: new Border.all(color: Colors.red, width: 4.0),
+          ),
+        ),
+      ),
+    );
   }
-
 }
-
-
